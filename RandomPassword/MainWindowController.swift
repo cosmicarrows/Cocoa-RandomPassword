8ec56fae994e8a7e627b5f6d4b6dbb0271c1e06b
Laurence Wingo <cosmicarrowsllc@cosmics-macbook-pro.local>
2018-05-02 11:30:13 -0400

    Implement MainViewController to show the UI of the Application
    In order to show the UI for the application we needed to add a new swift file and xib called MainViewController in which the application's AppDelegate will instantiate or call in order to assign it to the appDelegate's NSWindow object.  The NSWindow object is the main window of any Cocoa or Cocoa Touch application that presents the UI to the user.  Having MainViewController is a requirement.

---
+++ RandomPassword/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  RandomPassword
+//
+//  Created by Cosmic Arrows, LLC on 5/2/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
